3be3400d6bb23623e04238e07adab0c015bad4e6
Wendong Yao <atlasyao@uw.edu>
2020-03-04 15:54:46 -0800

    add code coverage

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
   [![Travis build status](https://travis-ci.org/atlasyao/happypackage.svg?branch=master)](https://travis-ci.org/atlasyao/happypackage)
   <!-- badges: end -->
 
+  <!-- badges: start -->
+  [![Codecov test coverage](https://codecov.io/gh/atlasyao/happypackage/branch/master/graph/badge.svg)](https://codecov.io/gh/atlasyao/happypackage?branch=master)
+  <!-- badges: end -->
+  
 haha
--- tests/testthat/test-f_to_c.R
+++ tests/testthat/test-f_to_c.R
@@ -1,9 +1,9 @@
 # within test-f_to_c.R
-test_that("message: f_to_c works mathmatically", {
+test_that("message: f_to_c works mathmatically", { # message to myself
   expect_equal(f_to_c(32), 0) # expect the two things to be equal
   expect_equal(f_to_c(212), 100)
 })
 
 test_that("non-numeric input throws error", {
-  expect_error(f_to_c("a string"))
+  expect_error(f_to_c("a string")) # expect error
 })
