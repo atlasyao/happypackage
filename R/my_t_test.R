b04f35b553ac55ccbc4bd7cff2cb9c207ca82b99
Wendong Yao <atlasyao@uw.edu>
2020-03-13 02:21:15 -0700

    adds documentation for my_t_test function

--- R/my_t_test.R
+++ R/my_t_test.R
@@ -1,3 +1,25 @@
+#' My t-test function
+#'
+#' This function performs a one sample t-test in R.
+#'
+#' @param x a non-empty numeric vector of data values.
+#' @param alternative a character string specifying the alternative hypothesis,
+#' must be one of "two.sided", "greater" or "less".
+#' @param mu a number indicating the true value of the mean
+#' (or difference in means if you are performing a two sample test).
+#'
+#' @return the t test results of x:
+#' a list includes,
+#' \code{test_stat}, the value of the test statistic;
+#' \code{df}, the degrees of freedom;
+#' \code{alternative} a character string describing the alternative hypothesis;
+#' \code{p_val}, the p-value for the test.
+#'
+#' @examples
+#' my_t_test(x = c(1:200), alternative = "less", mu = 25)
+#' my_t_test(c(1:100), greater, 10)
+#'
+#' @export
 my_t_test <- function(x, alternative, mu) {
   # send an error message if the alternative is unrecognizable
   if (!(alternative %in% c("two.sided", "less", "greater"))) {
